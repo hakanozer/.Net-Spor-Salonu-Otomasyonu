92b6c3c1594ef9e898a89063a5a0ce694337ef13
SedaDEM1R <sedademir.ds@gmail.com>
2016-01-08 17:57:24 +0200

    Şirket Bilgileri Düzenleme
    Şirket Bilgileri Düzenleme sekmesinde unvan, telefon, adres, mail, web,
    çalışma başlangıç, çalışma bitiş, şirket logosu ve tarih güncelleme gibi
    işlemler Entity Framework Teknolojisi  kullanılarak yapılmaktadır.

---
+++ sirketBilgileriDuzenle.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NetFitnessProject
+{
+    public partial class sirketBilgileriDuzenle : Screen
+    {
+        public sirketBilgileriDuzenle()
+        {
+            InitializeComponent();
+        }
+
+        public void Güncelleme()
+        {
+            try
+            {
+                using (var context = new sporEntities())
+                {
+                    sirketBilgileri dm = context.sirketBilgileris.FirstOrDefault(sil => sil.sirketID == id);
+                    if (dm != null)
+                    {
+                        if (String.IsNullOrEmpty(textBox1.Text))
+                        {
+                            MessageBox.Show("Lütfen Unvan Alanını Doldurunuz!");
+                            textBox1.Focus();
+                        }
+                        else
+                        {
+                            if (String.IsNullOrEmpty(textBox2.Text))
+                            {
+                                MessageBox.Show("Lütfen Telefon Alanını Doldurunuz!");
+                                textBox2.Focus();
+                            }
+                            else
+                            {
+                                if (String.IsNullOrEmpty(textBox3.Text))
+                                {
+                                    MessageBox.Show("Lütfen Adres Alanını Doldurunuz!");
+                                    textBox3.Focus();
+                                }
+                                else
+                                {
+                                    if (String.IsNullOrEmpty(textBox4.Text))
+                                    {
+                                        MessageBox.Show("Lütfen Mail Alanını Doldurunuz!");
+                                        textBox4.Focus();
+                                    }
+                                    else
+                                    {
+                                        if (String.IsNullOrEmpty(textBox5.Text))
+                                        {
+                                            MessageBox.Show("Lütfen Web Alanını Doldurunuz!");
+                                            textBox5.Focus();
+                                        }
+                                        else
+                                        {
+                                            if (String.IsNullOrEmpty(textBox6.Text))
+                                            {
+                                                MessageBox.Show("Lütfen Çalışma Başlangıç Alanını Doldurunuz!");
+                                                textBox6.Focus();
+                                            }
+                                            else
+                                            {
+                                                if (String.IsNullOrEmpty(textBox7.Text))
+                                                {
+                                                    MessageBox.Show("Lütfen Çalışma Bitiş Alanını Doldurunuz!");
+                                                    textBox7.Focus();
+                                                }
+                                                else
+                                                {
+                                                    if (String.IsNullOrEmpty(textBox8.Text))
+                                                    {
+                                                        MessageBox.Show("Lütfen Logo Alanını Doldurunuz!");
+                                                        textBox8.Focus();
+                                                    }
+                                                    else
+                                                    {
+                                                        if (String.IsNullOrEmpty(dateTimePicker1.Value.ToString()))
+                                                        {
+                                                            MessageBox.Show("Lütfen Tarih Alanını Doldurunuz!");
+                                                        }
+                                                        else
+                                                        {
+                                                            dm.unvan = textBox1.Text;
+                                                            dm.telefon = textBox2.Text;
+                                                            dm.adres = textBox3.Text;
+                                                            dm.mail = textBox4.Text;
+                                                            dm.web = textBox5.Text;
+                                                            dm.calismaBaslangic = textBox6.Text;
+                                                            dm.calismaBitis = textBox7.Text;
+                                                            dm.logo = textBox8.Text;
+                                                            dm.tarih = dateTimePicker1.Value;
+
+                                                            context.SaveChanges();
+                                                            Form1_Load(null, null);
+                                                            MessageBox.Show("Güncelleme işlemi başarılı!");
+                                                            textBox1.Clear();
+                                                            textBox2.Clear();
+                                                            textBox3.Clear();
+                                                            textBox4.Clear();
+                                                            textBox5.Clear();
+                                                            textBox6.Clear();
+                                                            textBox7.Clear();
+                                                            textBox8.Clear();
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }            
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lütfen Seçim Yapınız!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Güncelleme Hatası : " + ex);
+            }
+        }
+        
+    }
+}
---
+++ sirketBilgileriDuzenle.designer.cs
@@ -0,0 +1,309 @@
+﻿namespace NetFitnessProject
+{
+    partial class sirketBilgileriDuzenle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUnvan = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.lblAdres = new System.Windows.Forms.Label();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.lblWeb = new System.Windows.Forms.Label();
+            this.lblCalismaBaslangic = new System.Windows.Forms.Label();
+            this.lblCalismaBitis = new System.Windows.Forms.Label();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.grpSirketBilgileri = new System.Windows.Forms.GroupBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblTarih = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.grpSirketBilgileri.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblUnvan
+            // 
+            this.lblUnvan.AutoSize = true;
+            this.lblUnvan.Location = new System.Drawing.Point(41, 40);
+            this.lblUnvan.Name = "lblUnvan";
+            this.lblUnvan.Size = new System.Drawing.Size(55, 20);
+            this.lblUnvan.TabIndex = 0;
+            this.lblUnvan.Text = "Unvan";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(41, 72);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(62, 20);
+            this.lblTelefon.TabIndex = 1;
+            this.lblTelefon.Text = "Telefon";
+            // 
+            // lblAdres
+            // 
+            this.lblAdres.AutoSize = true;
+            this.lblAdres.Location = new System.Drawing.Point(41, 102);
+            this.lblAdres.Name = "lblAdres";
+            this.lblAdres.Size = new System.Drawing.Size(51, 20);
+            this.lblAdres.TabIndex = 2;
+            this.lblAdres.Text = "Adres";
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(41, 135);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(37, 20);
+            this.lblMail.TabIndex = 3;
+            this.lblMail.Text = "Mail";
+            // 
+            // lblWeb
+            // 
+            this.lblWeb.AutoSize = true;
+            this.lblWeb.Location = new System.Drawing.Point(41, 166);
+            this.lblWeb.Name = "lblWeb";
+            this.lblWeb.Size = new System.Drawing.Size(42, 20);
+            this.lblWeb.TabIndex = 4;
+            this.lblWeb.Text = "Web";
+            // 
+            // lblCalismaBaslangic
+            // 
+            this.lblCalismaBaslangic.AutoSize = true;
+            this.lblCalismaBaslangic.Location = new System.Drawing.Point(41, 197);
+            this.lblCalismaBaslangic.Name = "lblCalismaBaslangic";
+            this.lblCalismaBaslangic.Size = new System.Drawing.Size(138, 20);
+            this.lblCalismaBaslangic.TabIndex = 5;
+            this.lblCalismaBaslangic.Text = "Çalışma Başlangıç";
+            // 
+            // lblCalismaBitis
+            // 
+            this.lblCalismaBitis.AutoSize = true;
+            this.lblCalismaBitis.Location = new System.Drawing.Point(41, 230);
+            this.lblCalismaBitis.Name = "lblCalismaBitis";
+            this.lblCalismaBitis.Size = new System.Drawing.Size(99, 20);
+            this.lblCalismaBitis.TabIndex = 6;
+            this.lblCalismaBitis.Text = "Çalışma Bitiş";
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.Location = new System.Drawing.Point(41, 263);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(45, 20);
+            this.lblLogo.TabIndex = 7;
+            this.lblLogo.Text = "Logo";
+            // 
+            // grpSirketBilgileri
+            // 
+            this.grpSirketBilgileri.Controls.Add(this.textBox8);
+            this.grpSirketBilgileri.Controls.Add(this.button1);
+            this.grpSirketBilgileri.Controls.Add(this.dataGridView1);
+            this.grpSirketBilgileri.Controls.Add(this.btnGuncelle);
+            this.grpSirketBilgileri.Controls.Add(this.dateTimePicker1);
+            this.grpSirketBilgileri.Controls.Add(this.textBox7);
+            this.grpSirketBilgileri.Controls.Add(this.textBox6);
+            this.grpSirketBilgileri.Controls.Add(this.textBox5);
+            this.grpSirketBilgileri.Controls.Add(this.textBox4);
+            this.grpSirketBilgileri.Controls.Add(this.textBox3);
+            this.grpSirketBilgileri.Controls.Add(this.textBox2);
+            this.grpSirketBilgileri.Controls.Add(this.textBox1);
+            this.grpSirketBilgileri.Controls.Add(this.lblTelefon);
+            this.grpSirketBilgileri.Controls.Add(this.lblTarih);
+            this.grpSirketBilgileri.Controls.Add(this.lblLogo);
+            this.grpSirketBilgileri.Controls.Add(this.lblUnvan);
+            this.grpSirketBilgileri.Controls.Add(this.lblCalismaBitis);
+            this.grpSirketBilgileri.Controls.Add(this.lblAdres);
+            this.grpSirketBilgileri.Controls.Add(this.lblCalismaBaslangic);
+            this.grpSirketBilgileri.Controls.Add(this.lblMail);
+            this.grpSirketBilgileri.Controls.Add(this.lblWeb);
+            this.grpSirketBilgileri.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grpSirketBilgileri.Location = new System.Drawing.Point(62, 33);
+            this.grpSirketBilgileri.Name = "grpSirketBilgileri";
+            this.grpSirketBilgileri.Size = new System.Drawing.Size(658, 517);
+            this.grpSirketBilgileri.TabIndex = 8;
+            this.grpSirketBilgileri.TabStop = false;
+            this.grpSirketBilgileri.Text = "Şirket Bilgileri";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(248, 256);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(237, 26);
+            this.textBox8.TabIndex = 22;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(491, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 27);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 343);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(571, 150);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(520, 287);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(96, 40);
+            this.btnGuncelle.TabIndex = 17;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(248, 291);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(237, 26);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(248, 224);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(237, 26);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(248, 191);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(237, 26);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(248, 160);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(237, 26);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(248, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(237, 26);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(248, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(237, 26);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(248, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 26);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // lblTarih
+            // 
+            this.lblTarih.AutoSize = true;
+            this.lblTarih.Location = new System.Drawing.Point(41, 297);
+            this.lblTarih.Name = "lblTarih";
+            this.lblTarih.Size = new System.Drawing.Size(44, 20);
+            this.lblTarih.TabIndex = 7;
+            this.lblTarih.Text = "Tarih";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.grpSirketBilgileri);
+            this.Name = "Form1";
+            this.Text = "Ayarlar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpSirketBilgileri.ResumeLayout(false);
+            this.grpSirketBilgileri.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUnvan;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.Label lblAdres;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.Label lblWeb;
+        private System.Windows.Forms.Label lblCalismaBaslangic;
+        private System.Windows.Forms.Label lblCalismaBitis;
+        private System.Windows.Forms.Label lblLogo;
+        private System.Windows.Forms.GroupBox grpSirketBilgileri;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTarih;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
