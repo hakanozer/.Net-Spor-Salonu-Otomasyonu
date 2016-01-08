c1eb11790c4eb81b2cc129379386a326f79d27fa
edanharna <erhert@gmail.com>
2016-01-08 16:56:54 +0200

    Kategori Kontrol
    Projede kategori ekleme, silme ve güncelleme islemleri yapildi ve
    yapilan degisikliklerin
    listelenmesi görevini üstelendim. Görevimde Entity Framework, LINQ
    teknolojilerini kullandim.

---
+++ NetFitnessProject/kategori_Kontrol.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace NetFitnessProject
+{
+
+
+    public partial class kategori_Kontrol : Screen
+    {
+        public kategori_Kontrol()
+        {
+            InitializeComponent();
+        }
+
+
+        sporEntities db = new sporEntities();
+        int silID = -1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ( txtAdı.Text == "")
+                    throw new Exception("Kategori adı boş bırakılamaz");
+                kategoriler kt = new kategoriler();
+
+                kt.adi = txtAdı.Text;
+                kt.tarih = DateTime.Now;
+
+                db.kategorilers.Add(kt);
+                db.SaveChanges();
+                Form1_Load(null,null);
+            }
+            catch (Exception er)
+            {
+
+                MessageBox.Show("" + er.Message);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGetir();
+            silID = -1;
+            txtAdı.Text = "";
+            txtAdı.Focus();
+        }
+
+
+        //int did = -1;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count != 0 && silID != -1)
+            {
+                
+
+                //String id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                //did = Convert.ToInt32(id);
+                db.kategorilers.Remove(db.kategorilers.Find(silID));
+                try
+                {
+                    db.SaveChanges();
+                    Form1_Load(null, null);
+                    //button2_Click(null, null);
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Silme Hatası : " + ex);
+                }
+
+                //dataGetir();
+                //dataGridView1_DataBindingComplete(null,null);
+               
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count != 0 && silID != -1 && !string.IsNullOrEmpty(txtAdı.Text))
+            {
+                int id = Convert.ToInt32(dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value);
+                try
+                {
+                    var context = new sporEntities();
+                    kategoriler kt = context.kategorilers.FirstOrDefault(sil => sil.kategoriID == id);
+                    if (kt != null)
+                    {
+                        kt.adi = txtAdı.Text;
+                        kt.tarih = DateTime.Now;
+                        
+                        context.SaveChanges();
+                        Form1_Load(null,null);
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Alanlar boş bırakılamaz " );
+                }
+
+              
+               
+            }
+
+        }
+
+        private void dataGetir()
+        {
+            dataGridView1.DataSource = null;
+
+
+
+            using (sporEntities db2 = new sporEntities())
+            {
+                List<kategoriler> liste2 = new List<kategoriler>();
+
+                liste2 = db2.kategorilers.ToList();
+                if (liste2 != null)
+                {
+                    dataGridView1.DataSource = liste2;
+                }
+            }
+
+
+            //IEnumerable<kategoriler> liste = db.kategorilers;
+            //ArrayList ls = new ArrayList();
+            //foreach (var item in liste)
+            //{
+            //    kategoriler c = new kategoriler();
+            //    c.kategoriID = item.kategoriID;
+            //    c.adi = item.adi;
+            //    c.tarih = item.tarih;
+            //    //c.Picture = item.Picture;
+            //    ls.Add(c);
+            //}
+            //dataGridView1.DataSource = ls;
+
+
+        }
+
+
+
+        //private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        //{
+
+            
+        //    try
+        //    {
+        //        if (dataGridView1.Rows.Count == 0)
+        //            throw new Exception("Seçili alan yok");
+
+        //        txtAdı.Text = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[1].Value.ToString();
+        //        txtTarihi.Text = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[2].Value.ToString();
+        //    }
+        //    catch (Exception er)
+        //    {
+
+        //        MessageBox.Show(""+er.Message);
+
+        //    }
+
+
+        //}
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                silID = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            }
+            catch (Exception)
+            {
+
+                
+            } 
+         
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            dataGridView1.ClearSelection();
+        }
+
+        private void txtAdı_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        }
+    }
+
---
+++ NetFitnessProject/kategori_Kontrol.designer.cs
@@ -0,0 +1,158 @@
+﻿namespace NetFitnessProject
+{
+    partial class kategori_Kontrol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAdı = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(93, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(702, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SİL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(177, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "GÜNCELLE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(355, 81);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(417, 260);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // txtAdı
+            // 
+            this.txtAdı.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdı.Location = new System.Drawing.Point(93, 46);
+            this.txtAdı.Name = "txtAdı";
+            this.txtAdı.Size = new System.Drawing.Size(174, 22);
+            this.txtAdı.TabIndex = 4;
+            this.txtAdı.TextChanged += new System.EventHandler(this.txtAdı_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kategori Adı";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtAdı);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(21, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 151);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kategori Kontrol";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Kategoriler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.Controls.SetChildIndex(this.dataGridView1, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtAdı;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
