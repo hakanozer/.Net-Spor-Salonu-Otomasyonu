a90dbe39f541e7b7bb0417765d9e497887ecdd20
hulyasababli <hlyasababli@gmail.com>
2016-01-08 17:51:37 +0200

    PROJE YEDEKLEME
    Proje yedekelemede önem arz eden bölüm oluşabilecek veri kaybına karşı
    önlem alabilmektir. Ayrıca istenilen durumlarda da  arz edilen
    tabloların yedeğinide alabilme ayrıcalığı sunmaktadır.
    İki durum unsuru vardır; veritabanı yedeği   ‘.bak’  uzantısı ile
    kaydedilmektedir. Seçilen  tabloların yedeği  ise veritabanında
    oluşabilmektedir .

---
+++ NetFitnessProject/backUpKontrol.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace NetFitnessProject
+{
+    public partial class backUpKontrol : Screen
+    {
+        DB db = new DB();
+        public backUpKontrol()
+        {
+            InitializeComponent();
+        }
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            db.ac();
+           
+            try
+            {
+                foreach (Control radio in grpBackup.Controls)
+                {
+                    if (radio is RadioButton)
+                    {
+                        RadioButton rdb = (RadioButton)radio;
+                        if (!rdb.Checked)
+                        {
+                            MessageBox.Show("Lütfen seçim yapınız.");
+                            
+                        }
+                      
+                        else if (rdb.Checked)
+                        {
+                            SaveFileDialog sf = new SaveFileDialog();
+                            string FileName = "";
+                            sf.InitialDirectory = "";
+                            sf.FileName = "";
+                            sf.RestoreDirectory = true;
+                            if (sf.ShowDialog() == DialogResult.OK)
+                            {
+                                if (!string.IsNullOrEmpty(sf.FileName))
+                                {
+                                    SqlCommand cm;
+                                    cm = new SqlCommand(@"backup database " + rdb.Text + " to disk='" + sf.FileName + ".bak' with init, stats=10", db.conn);
+                                    cm.ExecuteNonQuery();
+
+                                    MessageBox.Show("Backup işlemi başarılı");
+
+                                }
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Lütfen dosya ismi veriniz");
+                            }
+                            db.kapat();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Veritabanı yedeği alınamadı !! :" + ex); ;
+            }
+        }
+
+        private void backUpKontrol_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NetFitnessProject/backUpKontrol.designer.cs
@@ -0,0 +1,282 @@
+﻿namespace NetFitnessProject
+{
+    partial class backUpKontrol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpBackup = new System.Windows.Forms.GroupBox();
+            this.radioButton11 = new System.Windows.Forms.RadioButton();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.grpYedek = new System.Windows.Forms.GroupBox();
+            this.btnYedek = new System.Windows.Forms.Button();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.grpBackup.SuspendLayout();
+            this.grpYedek.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBackup
+            // 
+            this.grpBackup.BackColor = System.Drawing.Color.Transparent;
+            this.grpBackup.Controls.Add(this.radioButton11);
+            this.grpBackup.Controls.Add(this.btnBackup);
+            this.grpBackup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grpBackup.Location = new System.Drawing.Point(12, 77);
+            this.grpBackup.Name = "grpBackup";
+            this.grpBackup.Size = new System.Drawing.Size(596, 100);
+            this.grpBackup.TabIndex = 0;
+            this.grpBackup.TabStop = false;
+            this.grpBackup.Text = "Veritabanı Yedeği";
+            // 
+            // radioButton11
+            // 
+            this.radioButton11.AutoSize = true;
+            this.radioButton11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton11.Location = new System.Drawing.Point(6, 41);
+            this.radioButton11.Name = "radioButton11";
+            this.radioButton11.Size = new System.Drawing.Size(53, 20);
+            this.radioButton11.TabIndex = 2;
+            this.radioButton11.TabStop = true;
+            this.radioButton11.Text = "spor";
+            this.radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.BackColor = System.Drawing.Color.Transparent;
+            this.btnBackup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBackup.Location = new System.Drawing.Point(138, 36);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(92, 30);
+            this.btnBackup.TabIndex = 1;
+            this.btnBackup.Text = "Backup Al";
+            this.btnBackup.UseVisualStyleBackColor = false;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // grpYedek
+            // 
+            this.grpYedek.BackColor = System.Drawing.Color.Transparent;
+            this.grpYedek.Controls.Add(this.btnYedek);
+            this.grpYedek.Controls.Add(this.radioButton10);
+            this.grpYedek.Controls.Add(this.radioButton9);
+            this.grpYedek.Controls.Add(this.radioButton8);
+            this.grpYedek.Controls.Add(this.radioButton7);
+            this.grpYedek.Controls.Add(this.radioButton6);
+            this.grpYedek.Controls.Add(this.radioButton5);
+            this.grpYedek.Controls.Add(this.radioButton4);
+            this.grpYedek.Controls.Add(this.radioButton3);
+            this.grpYedek.Controls.Add(this.radioButton2);
+            this.grpYedek.Controls.Add(this.radioButton1);
+            this.grpYedek.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grpYedek.Location = new System.Drawing.Point(12, 199);
+            this.grpYedek.Name = "grpYedek";
+            this.grpYedek.Size = new System.Drawing.Size(596, 182);
+            this.grpYedek.TabIndex = 1;
+            this.grpYedek.TabStop = false;
+            this.grpYedek.Text = " Tablo Yedeği";
+            // 
+            // btnYedek
+            // 
+            this.btnYedek.BackColor = System.Drawing.Color.Transparent;
+            this.btnYedek.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYedek.Location = new System.Drawing.Point(138, 143);
+            this.btnYedek.Name = "btnYedek";
+            this.btnYedek.Size = new System.Drawing.Size(92, 30);
+            this.btnYedek.TabIndex = 10;
+            this.btnYedek.Text = "Yedek Al";
+            this.btnYedek.UseVisualStyleBackColor = false;
+            this.btnYedek.Click += new System.EventHandler(this.btnYedek_Click);
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton10.Location = new System.Drawing.Point(138, 110);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(63, 20);
+            this.radioButton10.TabIndex = 9;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "uyeler";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton9.Location = new System.Drawing.Point(138, 87);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(102, 20);
+            this.radioButton9.TabIndex = 8;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "sirketBilgileri";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton8.Location = new System.Drawing.Point(138, 64);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(69, 20);
+            this.radioButton8.TabIndex = 7;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "satislar";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton7.Location = new System.Drawing.Point(138, 41);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(92, 20);
+            this.radioButton7.TabIndex = 6;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "programlar";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton6.Location = new System.Drawing.Point(138, 18);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(75, 20);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "paketler";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton5.Location = new System.Drawing.Point(6, 109);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(90, 20);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "kategoriler";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton4.Location = new System.Drawing.Point(6, 87);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(71, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "hocalar";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(98, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "demirbaslar";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(6, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "branslar";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(90, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "adminGiris";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // backUpKontrol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(784, 566);
+            this.Controls.Add(this.grpYedek);
+            this.Controls.Add(this.grpBackup);
+            this.Name = "backUpKontrol";
+            this.Text = "Yedekleme";
+            this.Load += new System.EventHandler(this.backUpKontrol_Load);
+            this.Controls.SetChildIndex(this.grpBackup, 0);
+            this.Controls.SetChildIndex(this.grpYedek, 0);
+            this.grpBackup.ResumeLayout(false);
+            this.grpBackup.PerformLayout();
+            this.grpYedek.ResumeLayout(false);
+            this.grpYedek.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBackup;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.GroupBox grpYedek;
+        private System.Windows.Forms.Button btnYedek;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton11;
+    }
+}
+
