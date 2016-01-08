7c9279357a19a53e8d413117caa71e0189b285e8
handevural <handevural5@gmail.com>
2016-01-08 18:07:40 +0200

    Şifre Deiştirme
    Şifre değiştirme kısmından, sisteme giriş yapan kişinin şifre
    güncellemesi  Entity Framework Teknolojisi  kullanılarak yapılmaktadır.

--- SifreDegis.cs
+++ SifreDegis.cs
@@ -15,12 +15,58 @@ namespace NetFitnessProject
 {
     public partial class SifreDegis : Screen
     {
+        sporEntities sp = new sporEntities();
+        
         public SifreDegis()
         {
             InitializeComponent();
         }
-        
-        
+        int idd = 1;
+        private void btnDegistir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var context = new sporEntities())
+                {
+                    adminGiri sp = context.adminGiris.Find(idd);
+                    if (txtmail.Text == sp.mail)
+                    {
+                     if (MD5Olustur(txtEskiSifre.Text) == sp.sifre.ToString())
+                     {
+                         if (txtYeniSifre.Text == txtYeniSifre2.Text)
+                         {
+                             adminGiri dm = context.adminGiris.FirstOrDefault(sil => sil.adminID == idd);
+                              if (dm != null)
+                              {
+                                  dm.sifre = MD5Olustur(txtYeniSifre.Text);
+                                  context.SaveChanges();
+                                  MessageBox.Show("Şifre Değiştirme Başarılı!");
+                              }
+                          }
+                         else
+                         {
+                              MessageBox.Show("Yeni Şifreleriniz Uyuşmuyor!");
+                              temizle();
+                         }
+                      }
+                     else
+                     {
+                         MessageBox.Show("Eski Şifrenizi Yanlış Girdiniz!");
+                         temizle();
+                      }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Mail Adresinizi Yanlış Girdiniz!");
+                        temizle();
+                    }
+                 }
+             }
+                catch (Exception ex)
+             {
+                MessageBox.Show("Şifre Değiştirme Hatası" + ex);
+                }
+           }
         public String MD5Olustur(String input)
         {
             MD5 md5Hasher = MD5.Create();
@@ -32,6 +78,6 @@ namespace NetFitnessProject
             }
             return sBuilder.ToString();
         }
-        
+      
     }
 }
