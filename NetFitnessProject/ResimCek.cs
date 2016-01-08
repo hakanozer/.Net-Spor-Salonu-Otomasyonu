a1be9f76baacf26a28bd31dd19e2caa7198800a2
ramazanguclu <ramazanguclu33@gmail.com>
2016-01-08 16:39:31 +0200

    Fotoğraf Çekme
    Bu formda üyenin fotoğrafının güncellemesi sağlandı. Formda
    veritabanında
    kayıtlı olan üyeler listelendi. Kullanıcı seçtiği üyenin fofoğrafını
    silebilir
    ve güncelleyebilir. Formun alt kısmında fotoğrafın güncellenebilmesi
    için
    bilgisayarda kayıtlı olan web cam cihazları listelendi. Kullanıcı
    istediği
    kamera aygıtını seçip ardından 'çek' tuşuna basarak üyenin fotoğrafını
    güncelleyebilir.
    Kullanılan Teknolojiler: Ado.Net Entity Framework, Aforge.Net Framework

---
+++ NetFitnessProject/ResimCek.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.IO;
+
+namespace NetFitnessProject
+{
+    public partial class ResimCek : Screen
+    {
+        private FilterInfoCollection webcam = null;             //webcam isminde tanımladığımız değişken bilgisayara kaç kamera bağlıysa onları tutan bir dizi. 
+        private VideoCaptureDevice cam;                         //cam ise bizim kullanacağımız aygıt.
+        string varsayilanPath = "resimler\\photo.jpg";
+        bool deger = true;
+        bool guncellensinmi = false;
+        bool uyeFotoSilinsinmi = false;
+        bool varsayilanPathmi = false;
+        int seciliIndex = 0;
+        List<uyeler> uyeListesi = null;
+        public ResimCek()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);//webcam dizisine mevcut kameraları dolduruyoruz.
+            if (webcam != null)
+            {
+                foreach (FilterInfo videocapturedevice in webcam)
+                    comboBox1.Items.Add(videocapturedevice.Name);//kameraları combobox a dolduruyoruz.
+
+                comboBox1.SelectedIndex = 0;
+                btnBaslat_Click(null, null);
+            }
+            else
+                MessageBox.Show("Kamera Aygıtı Bulunamadı!");
+            Listele();
+            lstUyeler_SelectedIndexChanged(null, null);
+        }
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            if (deger && webcam != null)
+            {
+                try
+                {
+                    cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString); //başlaya basıldığıdnda yukarda tanımladığımız cam değişkenine                                                                                           cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);                      //comboboxta seçilmş olan kamerayı atıyoruz.
+                    cam.Start();                                    //kamerayı başlatıyoruz.
+                    
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("resim çekme hatası"+ex);
+                }
+
+                deger = false;  
+            }
+        }
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+                Bitmap bit = (Bitmap)eventArgs.Frame.Clone();//kısaca bu eventta kameradan alınan görüntüyü picturebox a atıyoruz.
+                pictureBox1.Image = bit;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kameradan görüntü okuma hatası: " + ex);
+            }
+        }
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            if (!deger && webcam != null)
+            {
+                cam.Stop();
+                deger = true;
+            }
+        }
+        private void btnCek_Click(object sender, EventArgs e)
+        {
+            if (webcam != null)
+            {
+                guncellensinmi = true;
+                pictureBox2.Image = pictureBox1.Image;//foto çek denildiği zaman picturebox1 taki o an ki görüntüyü picturebox2 ye atıyoruz.
+            }
+        }
+        private void btnTmizleKamera_Click(object sender, EventArgs e)
+        {
+            guncellensinmi = false;
+            pictureBox2.Load(varsayilanPath);
+        }
+        private void btnSilUye_Click(object sender, EventArgs e)
+        {
+            if (!varsayilanPathmi && uyeListesi != null)
+            {
+                DialogResult dialogResult = MessageBox.Show("Silmek İstediğinizden Emin Misiniz?", "Üye Fotoğrafı Silme ", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    uyeFotoSilinsinmi = true;
+                    seciliIndex = lstUyeler.SelectedIndex;
+                    if (ResimGuncelle(Convert.ToInt32(uyeListesi[lstUyeler.SelectedIndex].uyeID.ToString())))
+                        MessageBox.Show("Silme İşlemi Başarılı!");
+                    else
+                        MessageBox.Show("Silme İşlemi Başarısız!");
+                }
+            }
+            else
+                MessageBox.Show("Silinecek Bir Fotoğraf Yok!");
+        }
+        private void btnGuncelleFoto_Click(object sender, EventArgs e)
+        {
+            if (uyeListesi != null)
+            {
+                if (guncellensinmi)
+                {
+                    DialogResult dialogResult = MessageBox.Show("Güncellemek İstediğinizden Emin Misiniz?", "Üye Fotoğrafı Güncelleme", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        seciliIndex = lstUyeler.SelectedIndex;
+                        if (ResimGuncelle(Convert.ToInt32(uyeListesi[lstUyeler.SelectedIndex].uyeID.ToString())))
+                            MessageBox.Show("Üye Fotoğrafı Güncellendi!");
+                        else
+                            MessageBox.Show("Güncelleme İşlemi Başarısız!");
+                    }
+                }
+                else
+                    MessageBox.Show("Lütfen Bir Fotoğraf Çekiniz!");
+            }
+
+            else
+                MessageBox.Show("Güncellenecek Bir Fotoğraf Yok!");
+        }
+        private void lstUyeler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (uyeListesi != null)
+            {
+                txtResimYolu.Text = uyeListesi[lstUyeler.SelectedIndex].resim.ToString();
+                if (varsayilanPath == txtResimYolu.Text || string.IsNullOrEmpty(txtResimYolu.Text))
+                {
+                    varsayilanPathmi = true;
+                    pictureBox3.Load(varsayilanPath);
+                }
+                else
+                {
+                    try
+                    {
+                        varsayilanPathmi = false;
+                        pictureBox3.Load(uyeListesi[lstUyeler.SelectedIndex].resim.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        pictureBox3.Load(varsayilanPath);
+                        varsayilanPathmi = true;
+                        if (ex is FileNotFoundException)
+                            MessageBox.Show(txtResimYolu.Text + " İsimli Dosya Bulunamıyor. Silinmiş veya Başka Bir Yere Taşınmış Olabilir! Lütfen Yeni Bir Fotoğraj Çekiniz!");
+                    }
+                }
+            }
+        }
+        private void Listele()
+        {
+            using (sporEntities entity = new sporEntities())
+            {
+                uyeListesi = entity.uyelers.ToList();
+                if (uyeListesi != null)
+                {
+                    lstUyeler.DataSource = uyeListesi;
+                    lstUyeler.SelectedIndex = seciliIndex;
+                }
+                else
+                {
+                    varsayilanPathmi = true;
+                    MessageBox.Show("Veritabanında Hiçbir Üye Kaydı Bulunmamaktadır!");
+                }
+            }
+        }
+        private bool ResimGuncelle(int id)
+        {
+            bool deger = false;
+            using (sporEntities entity = new sporEntities())
+            {
+                uyeler uye = (from nesne in entity.uyelers
+                              where nesne.uyeID == id
+                              select nesne).FirstOrDefault();
+                if (uye != null)
+                {
+                    if (ResimSil(uye.resim))
+                    {
+                        if (uyeFotoSilinsinmi)
+                        {
+                            uye.resim = varsayilanPath;
+                            uyeFotoSilinsinmi = false;
+                        }
+                        else
+                        {
+                            pictureBox2.Image.Save("resimler\\" + id + ".jpg"); // klasore kayıt yapılıyor.
+                            uye.resim = "resimler\\" + id + ".jpg"; // ardından veritabanı güncelleniyor.
+                        }
+                        entity.SaveChanges();
+                        deger = true;
+                    }
+                }
+                else
+                    MessageBox.Show(string.Format("{0} ID numaralı üye bulunamadı!", id), "HATA");
+            }
+            Listele();
+            return deger;
+        }
+        private bool ResimSil(string path)
+        {
+            bool deger = false;
+            try
+            {
+                if (path != varsayilanPath && !string.IsNullOrEmpty(path))
+                {
+                    pictureBox3.Load(varsayilanPath);
+                    File.Delete(path);
+                    deger = true;
+                }
+                else
+                    deger = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return deger;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            btnDurdur_Click(null, null);
+        }
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void lstUyeler_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (uyeListesi != null)
+            {
+                e.DrawBackground();
+                e.Graphics.DrawString(uyeListesi[e.Index].uyeID.ToString(), lstUyeler.Font, Brushes.Black, new PointF(e.Bounds.Left, e.Bounds.Top));
+                e.Graphics.DrawString(uyeListesi[e.Index].tcNo, lstUyeler.Font, Brushes.Black, new PointF(e.Bounds.Left + (40), e.Bounds.Top));
+                e.Graphics.DrawString(uyeListesi[e.Index].adi.ToUpper().Trim(), lstUyeler.Font, Brushes.Black, new PointF(e.Bounds.Left + (135), e.Bounds.Top));
+                e.Graphics.DrawString(uyeListesi[e.Index].soyadi.ToUpper().Trim(), lstUyeler.Font, Brushes.Black, new PointF(e.Bounds.Left + (220), e.Bounds.Top));
+            }
+        }
+    }
+}
---
+++ NetFitnessProject/ResimCek.designer.cs
@@ -0,0 +1,291 @@
+﻿namespace NetFitnessProject
+{
+    partial class ResimCek
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResimCek));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnCek = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.btnSilKamera = new System.Windows.Forms.Button();
+            this.btnGuncelleFoto = new System.Windows.Forms.Button();
+            this.txtResimYolu = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblBaslik = new System.Windows.Forms.Label();
+            this.btnSilUye = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lstUyeler = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(29, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(313, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaslat.Location = new System.Drawing.Point(348, 61);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(89, 30);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "BAŞLAT";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnCek
+            // 
+            this.btnCek.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCek.Location = new System.Drawing.Point(348, 102);
+            this.btnCek.Name = "btnCek";
+            this.btnCek.Size = new System.Drawing.Size(89, 30);
+            this.btnCek.TabIndex = 2;
+            this.btnCek.Text = "ÇEK";
+            this.btnCek.UseVisualStyleBackColor = true;
+            this.btnCek.Click += new System.EventHandler(this.btnCek_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDurdur.Location = new System.Drawing.Point(348, 143);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(89, 30);
+            this.btnDurdur.TabIndex = 3;
+            this.btnDurdur.Text = "DURDUR";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(313, 196);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(520, 101);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(142, 146);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.btnCikis);
+            this.groupBox1.Controls.Add(this.btnSilKamera);
+            this.groupBox1.Controls.Add(this.btnGuncelleFoto);
+            this.groupBox1.Controls.Add(this.txtResimYolu);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.btnBaslat);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.btnCek);
+            this.groupBox1.Controls.Add(this.btnDurdur);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(25, 286);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(734, 257);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fotoğraf Çek";
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikis.Location = new System.Drawing.Point(348, 184);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(89, 30);
+            this.btnCikis.TabIndex = 9;
+            this.btnCikis.Text = "ÇIKIŞ";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // btnSilKamera
+            // 
+            this.btnSilKamera.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSilKamera.Location = new System.Drawing.Point(484, 51);
+            this.btnSilKamera.Name = "btnSilKamera";
+            this.btnSilKamera.Size = new System.Drawing.Size(80, 30);
+            this.btnSilKamera.TabIndex = 8;
+            this.btnSilKamera.Text = "TEMİZLE";
+            this.btnSilKamera.UseVisualStyleBackColor = true;
+            this.btnSilKamera.Click += new System.EventHandler(this.btnTmizleKamera_Click);
+            // 
+            // btnGuncelleFoto
+            // 
+            this.btnGuncelleFoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelleFoto.Location = new System.Drawing.Point(606, 51);
+            this.btnGuncelleFoto.Name = "btnGuncelleFoto";
+            this.btnGuncelleFoto.Size = new System.Drawing.Size(93, 30);
+            this.btnGuncelleFoto.TabIndex = 7;
+            this.btnGuncelleFoto.Text = "GÜNCELLE";
+            this.btnGuncelleFoto.UseVisualStyleBackColor = true;
+            this.btnGuncelleFoto.Click += new System.EventHandler(this.btnGuncelleFoto_Click);
+            // 
+            // txtResimYolu
+            // 
+            this.txtResimYolu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtResimYolu.Location = new System.Drawing.Point(484, 23);
+            this.txtResimYolu.Name = "txtResimYolu";
+            this.txtResimYolu.ReadOnly = true;
+            this.txtResimYolu.Size = new System.Drawing.Size(215, 22);
+            this.txtResimYolu.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.lblBaslik);
+            this.groupBox2.Controls.Add(this.btnSilUye);
+            this.groupBox2.Controls.Add(this.pictureBox3);
+            this.groupBox2.Controls.Add(this.lstUyeler);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(25, 62);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(734, 208);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Üye Fotoğrafları";
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.AutoSize = true;
+            this.lblBaslik.BackColor = System.Drawing.Color.Transparent;
+            this.lblBaslik.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBaslik.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBaslik.Location = new System.Drawing.Point(33, 30);
+            this.lblBaslik.MinimumSize = new System.Drawing.Size(310, 30);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(310, 30);
+            this.lblBaslik.TabIndex = 13;
+            this.lblBaslik.Text = "ID     TC NO        AD             SOYAD";
+            this.lblBaslik.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnSilUye
+            // 
+            this.btnSilUye.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSilUye.Location = new System.Drawing.Point(556, 167);
+            this.btnSilUye.Name = "btnSilUye";
+            this.btnSilUye.Size = new System.Drawing.Size(70, 30);
+            this.btnSilUye.TabIndex = 12;
+            this.btnSilUye.Text = "SİL";
+            this.btnSilUye.UseVisualStyleBackColor = true;
+            this.btnSilUye.Click += new System.EventHandler(this.btnSilUye_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(520, 30);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(142, 131);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lstUyeler
+            // 
+            this.lstUyeler.BackColor = System.Drawing.SystemColors.Control;
+            this.lstUyeler.ColumnWidth = 50;
+            this.lstUyeler.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.lstUyeler.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstUyeler.FormattingEnabled = true;
+            this.lstUyeler.ItemHeight = 15;
+            this.lstUyeler.Location = new System.Drawing.Point(33, 61);
+            this.lstUyeler.Name = "lstUyeler";
+            this.lstUyeler.Size = new System.Drawing.Size(309, 139);
+            this.lstUyeler.TabIndex = 10;
+            this.lstUyeler.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lstUyeler_DrawItem);
+            this.lstUyeler.SelectedIndexChanged += new System.EventHandler(this.lstUyeler_SelectedIndexChanged);
+            // 
+            // ResimCek
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ResimCek";
+            this.Text = "Kamera";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.groupBox2, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnCek;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSilKamera;
+        private System.Windows.Forms.Button btnGuncelleFoto;
+        private System.Windows.Forms.TextBox txtResimYolu;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSilUye;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ListBox lstUyeler;
+        private System.Windows.Forms.Label lblBaslik;
+        private System.Windows.Forms.Button btnCikis;
+    }
+}
+
