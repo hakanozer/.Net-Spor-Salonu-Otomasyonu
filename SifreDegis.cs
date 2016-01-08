e5bbb6f58b097aef84fa64e0167bf7145da0b6bd
SedaDEM1R <sedademir.ds@gmail.com>
2016-01-08 18:01:30 +0200

    Şifre Değiştirme
    Şifreler MD5 şifreleme fonksiyonları kullanılarak oluşturulmuştur.

---
+++ SifreDegis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace NetFitnessProject
+{
+    public partial class SifreDegis : Screen
+    {
+        public SifreDegis()
+        {
+            InitializeComponent();
+        }
+        
+        
+        public String MD5Olustur(String input)
+        {
+            MD5 md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+        
+    }
+}
---
+++ SifreDegis.designer.cs
@@ -0,0 +1,177 @@
+﻿namespace NetFitnessProject
+{
+    partial class SifreDegis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDegistir = new System.Windows.Forms.Button();
+            this.txtYeniSifre = new System.Windows.Forms.TextBox();
+            this.txtEskiSifre = new System.Windows.Forms.TextBox();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.lblYeniSifre2 = new System.Windows.Forms.Label();
+            this.lblYeniSifre = new System.Windows.Forms.Label();
+            this.lblEskiSifre = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtYeniSifre2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDegistir
+            // 
+            this.btnDegistir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDegistir.Location = new System.Drawing.Point(276, 181);
+            this.btnDegistir.Name = "btnDegistir";
+            this.btnDegistir.Size = new System.Drawing.Size(90, 34);
+            this.btnDegistir.TabIndex = 13;
+            this.btnDegistir.Text = "DEĞİŞTİR";
+            this.btnDegistir.UseVisualStyleBackColor = true;
+            this.btnDegistir.Click += new System.EventHandler(this.btnDegistir_Click);
+            // 
+            // txtYeniSifre
+            // 
+            this.txtYeniSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYeniSifre.Location = new System.Drawing.Point(149, 111);
+            this.txtYeniSifre.Name = "txtYeniSifre";
+            this.txtYeniSifre.Size = new System.Drawing.Size(217, 22);
+            this.txtYeniSifre.TabIndex = 12;
+            // 
+            // txtEskiSifre
+            // 
+            this.txtEskiSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtEskiSifre.Location = new System.Drawing.Point(149, 70);
+            this.txtEskiSifre.Name = "txtEskiSifre";
+            this.txtEskiSifre.Size = new System.Drawing.Size(217, 22);
+            this.txtEskiSifre.TabIndex = 11;
+            // 
+            // txtmail
+            // 
+            this.txtmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtmail.Location = new System.Drawing.Point(149, 29);
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(217, 22);
+            this.txtmail.TabIndex = 10;
+            // 
+            // lblYeniSifre2
+            // 
+            this.lblYeniSifre2.AutoSize = true;
+            this.lblYeniSifre2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYeniSifre2.Location = new System.Drawing.Point(32, 152);
+            this.lblYeniSifre2.Name = "lblYeniSifre2";
+            this.lblYeniSifre2.Size = new System.Drawing.Size(108, 16);
+            this.lblYeniSifre2.TabIndex = 9;
+            this.lblYeniSifre2.Text = "Yeni Şifre Tekrar";
+            // 
+            // lblYeniSifre
+            // 
+            this.lblYeniSifre.AutoSize = true;
+            this.lblYeniSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYeniSifre.Location = new System.Drawing.Point(32, 111);
+            this.lblYeniSifre.Name = "lblYeniSifre";
+            this.lblYeniSifre.Size = new System.Drawing.Size(65, 16);
+            this.lblYeniSifre.TabIndex = 8;
+            this.lblYeniSifre.Text = "Yeni Şifre";
+            // 
+            // lblEskiSifre
+            // 
+            this.lblEskiSifre.AutoSize = true;
+            this.lblEskiSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEskiSifre.Location = new System.Drawing.Point(32, 70);
+            this.lblEskiSifre.Name = "lblEskiSifre";
+            this.lblEskiSifre.Size = new System.Drawing.Size(64, 16);
+            this.lblEskiSifre.TabIndex = 7;
+            this.lblEskiSifre.Text = "Eski Şifre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Mail";
+            // 
+            // txtYeniSifre2
+            // 
+            this.txtYeniSifre2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYeniSifre2.Location = new System.Drawing.Point(149, 152);
+            this.txtYeniSifre2.Name = "txtYeniSifre2";
+            this.txtYeniSifre2.Size = new System.Drawing.Size(217, 22);
+            this.txtYeniSifre2.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtmail);
+            this.groupBox1.Controls.Add(this.txtYeniSifre2);
+            this.groupBox1.Controls.Add(this.lblEskiSifre);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblYeniSifre);
+            this.groupBox1.Controls.Add(this.btnDegistir);
+            this.groupBox1.Controls.Add(this.lblYeniSifre2);
+            this.groupBox1.Controls.Add(this.txtYeniSifre);
+            this.groupBox1.Controls.Add(this.txtEskiSifre);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(438, 231);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Şifre İşlemleri";
+            // 
+            // SifreDegis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 566);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "SifreDegis";
+            this.Text = "Form1";
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button btnDegistir;
+        private System.Windows.Forms.TextBox txtYeniSifre;
+        private System.Windows.Forms.TextBox txtEskiSifre;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.Label lblYeniSifre2;
+        private System.Windows.Forms.Label lblYeniSifre;
+        private System.Windows.Forms.Label lblEskiSifre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtYeniSifre2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
