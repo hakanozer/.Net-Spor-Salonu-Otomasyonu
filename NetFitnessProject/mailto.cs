d77406996845878d265ff859740eeb95afcd6fac
Okan Yerşen <okanyersen@gmail.com>
2016-01-08 17:39:10 +0200

    Mail
    Mail sistemi

---
+++ NetFitnessProject/mailto.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace NetFitnessProject
+{
+    public partial class mailto : Screen
+    {
+        MailMessage mail = new MailMessage();
+
+        public mailto()
+        {
+            InitializeComponent();
+        }
+        private void Mail_Load(object sender, EventArgs e)
+        {
+           
+            timer1.Enabled = true;
+            dataGetir();
+
+        }
+        private void btGonder_Click(object sender, EventArgs e)
+        {
+            bool kontrol = EmailKontrol(textBox1.Text);
+
+            if (kontrol == false)
+            {
+                errorProvider1.SetError(textBox1, "Geçersiz email format");
+                MessageBox.Show("Geçersiz email formatı girdiniz");
+                textBox1.Text = null;
+
+            }
+            else
+            {
+                SmtpClient sc = new SmtpClient();
+                sc.Port = 587;
+                sc.Host = "smtp.gmail.com";
+                sc.EnableSsl = true;
+
+                string kime = textBox1.Text;
+                string konu = textBox2.Text;
+                string icerik = textBox3.Text;
+                try
+                {
+                    sc.Credentials = new NetworkCredential("hakanilefitness@gmail.com", "fitnessilehakan");
+
+                    mail.From = new MailAddress("hakanilefitness@gmail.com", "Hakan Fitness");
+                    mail.To.Add(kime);
+                    mail.Subject = konu;
+                    mail.IsBodyHtml = true;
+                    mail.Body = icerik;
+
+                    sc.Send(mail);
+                    MessageBox.Show("Mail Gönderildi:\n\n");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Mail Gönderilemedi:\n\n" + ex);
+                }
+            }
+
+            
+
+        }
+
+        int kontrol = 0;
+        private void btEkle_Click(object sender, EventArgs e)
+        {
+            string DosyaYolu = null;
+            kontrol++;
+
+            OpenFileDialog dosya = new OpenFileDialog();
+            dosya.ShowDialog();
+            DosyaYolu = dosya.FileName;
+            textBox4.Text += dosya.SafeFileName + "\r\n";
+
+            try
+            {
+                mail.Attachments.Add(new Attachment(DosyaYolu));
+                label4.Text = kontrol + " Dosya Eklendi.";
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Dosya Eklenemedi Hatası:\n" + ex);
+            }
+        }
+        static bool EmailKontrol(string inputEmail)
+        {
+            const string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+
+            return (new Regex(strRegex)).IsMatch(inputEmail);
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            temizle(this);
+        }
+        private void temizle(Control ctl)
+        {
+            mail = new MailMessage();
+            label4.Text = "Dosya Eklenmedi...";
+            foreach (Control c in ctl.Controls)
+            {
+                if (c is TextBox)
+                {
+                    ((TextBox)c).Clear();
+                }
+
+                if (c.Controls.Count > 0)
+                {
+                    temizle(c);
+                }
+            }
+            textBox1.Focus();
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label5.Text = DateTime.Now.ToString();
+        }
+
+        List<string> list = new List<string>();
+        private void dataGetir()
+        {
+            try
+            {
+                var context = new sporEntities();
+
+                foreach (var item in context.proMail())
+                {
+                   // checkedListBox1.Items.Add(item.adi.ToUpperInvariant() + " " + item.soyadi.ToUpper());
+                    list.Add(item.mail);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Uye Yükleme Hatası" + ex);
+            }
+        }
+
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+           
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+//textBox1.Text = list[checkedListBox1.SelectedIndex];
+        }
+    }
+}
---
+++ NetFitnessProject/mailto.designer.cs
@@ -0,0 +1,244 @@
+﻿namespace NetFitnessProject
+{
+    partial class mailto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btEkle = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btGonder = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(319, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(269, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "VAZGEÇ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(108, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Dosya Eklenmedi...";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Location = new System.Drawing.Point(105, 305);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(325, 55);
+            this.textBox4.TabIndex = 10;
+            // 
+            // btEkle
+            // 
+            this.btEkle.BackColor = System.Drawing.Color.Transparent;
+            this.btEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btEkle.Location = new System.Drawing.Point(22, 320);
+            this.btEkle.Name = "btEkle";
+            this.btEkle.Size = new System.Drawing.Size(65, 30);
+            this.btEkle.TabIndex = 4;
+            this.btEkle.Text = "Dosya Ekle";
+            this.btEkle.UseVisualStyleBackColor = false;
+            this.btEkle.Click += new System.EventHandler(this.btEkle_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(77, 112);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(353, 186);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(77, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(353, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(77, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(19, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mesaj";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(19, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Konu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(19, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Alıcı";
+            // 
+            // btGonder
+            // 
+            this.btGonder.BackColor = System.Drawing.Color.Transparent;
+            this.btGonder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btGonder.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btGonder.Location = new System.Drawing.Point(355, 385);
+            this.btGonder.Name = "btGonder";
+            this.btGonder.Size = new System.Drawing.Size(80, 30);
+            this.btGonder.TabIndex = 5;
+            this.btGonder.Text = "GÖNDER";
+            this.btGonder.UseVisualStyleBackColor = false;
+            this.btGonder.Click += new System.EventHandler(this.btGonder_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btGonder);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btEkle);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(19, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(468, 434);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mail";
+            // 
+            // mailto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "mailto";
+            this.Text = "Mail";
+            this.Load += new System.EventHandler(this.Mail_Load);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btGonder;
+        private System.Windows.Forms.Button btEkle;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
