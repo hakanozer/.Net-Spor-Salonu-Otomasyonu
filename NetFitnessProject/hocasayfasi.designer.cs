c4ffb3ed2f660bb406b61d879557bb930d4f72aa
emine gün <emine1gun@gmail.com>
2016-01-08 18:19:00 +0200

    Hoca Sayfası
    Hocanın işlemleri gerçekleştirdiği ve kendine ait üyeleri görüntülediği
    kısımdır.

---
+++ NetFitnessProject/hocasayfasi.designer.cs
@@ -0,0 +1,176 @@
+﻿namespace NetFitnessProject
+{
+    partial class hocasayfasi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(hocasayfasi));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnUyeler = new System.Windows.Forms.Button();
+            this.btnPrgramOlustur = new System.Windows.Forms.Button();
+            this.btnTurnike = new System.Windows.Forms.Button();
+            this.btnHocaAyar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 152);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(760, 356);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(657, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "                                             HOŞGELDİNİZ SN.ADMİN                " +
+    "                          ";
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCikis.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnCikis.Location = new System.Drawing.Point(692, 12);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(80, 35);
+            this.btnCikis.TabIndex = 1;
+            this.btnCikis.Text = "Çıkış";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(439, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Raporlama";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUyeler
+            // 
+            this.btnUyeler.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUyeler.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnUyeler.Location = new System.Drawing.Point(201, 12);
+            this.btnUyeler.Name = "btnUyeler";
+            this.btnUyeler.Size = new System.Drawing.Size(80, 35);
+            this.btnUyeler.TabIndex = 0;
+            this.btnUyeler.Text = "Üyeler";
+            this.btnUyeler.UseVisualStyleBackColor = true;
+            this.btnUyeler.Click += new System.EventHandler(this.btnUyeler_Click);
+            // 
+            // btnPrgramOlustur
+            // 
+            this.btnPrgramOlustur.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrgramOlustur.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnPrgramOlustur.Location = new System.Drawing.Point(12, 12);
+            this.btnPrgramOlustur.Name = "btnPrgramOlustur";
+            this.btnPrgramOlustur.Size = new System.Drawing.Size(150, 35);
+            this.btnPrgramOlustur.TabIndex = 0;
+            this.btnPrgramOlustur.Text = "Program Oluşturma";
+            this.btnPrgramOlustur.UseVisualStyleBackColor = true;
+            this.btnPrgramOlustur.Click += new System.EventHandler(this.btnPrgramOlustur_Click);
+            // 
+            // btnTurnike
+            // 
+            this.btnTurnike.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTurnike.Location = new System.Drawing.Point(573, 12);
+            this.btnTurnike.Name = "btnTurnike";
+            this.btnTurnike.Size = new System.Drawing.Size(80, 35);
+            this.btnTurnike.TabIndex = 5;
+            this.btnTurnike.Text = "Turnike";
+            this.btnTurnike.UseVisualStyleBackColor = true;
+            this.btnTurnike.Click += new System.EventHandler(this.btnTurnike_Click);
+            // 
+            // btnHocaAyar
+            // 
+            this.btnHocaAyar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHocaAyar.Location = new System.Drawing.Point(320, 12);
+            this.btnHocaAyar.Name = "btnHocaAyar";
+            this.btnHocaAyar.Size = new System.Drawing.Size(80, 35);
+            this.btnHocaAyar.TabIndex = 7;
+            this.btnHocaAyar.Text = "Ayarlar";
+            this.btnHocaAyar.UseVisualStyleBackColor = true;
+            this.btnHocaAyar.Click += new System.EventHandler(this.btnHocaAyar_Click);
+            // 
+            // hocasayfasi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.btnHocaAyar);
+            this.Controls.Add(this.btnTurnike);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.btnUyeler);
+            this.Controls.Add(this.btnPrgramOlustur);
+            this.Name = "hocasayfasi";
+            this.Text = "hocasayfasi";
+            this.Load += new System.EventHandler(this.hocasayfasi_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrgramOlustur;
+        private System.Windows.Forms.Button btnUyeler;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTurnike;
+        private System.Windows.Forms.Button btnHocaAyar;
+    }
+}
