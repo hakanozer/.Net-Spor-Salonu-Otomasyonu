bf415f3072c319f60fc1e437b733a8c376108020
Volga Barış Civelek <volgabarcivelek@gmail.com>
2016-01-08 17:53:28 +0200

    Hoca Ekranı
    branşlara göre hocalar eklenir
    hoca bilgileri düzenlenir
    hoca veritabanından silinebilr
    hocanın resmi eklenir ve değiştirilir
    branşlara göre seçili hoca bilgileri data grid view da görüntülenir.
    seçili hoca resmi picture box ta görüntülenir

---
+++ NetFitnessProject/Hoca Ekrani.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace KullaniciGirisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(760, 322);
+            this.dataGridView1.TabIndex = 0;            
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(602, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "HOCA EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(602, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "HOCA SİL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(602, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "DÜZENLE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(328, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "Bir branş seçiniz...";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Hoca Branşı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(59, 71);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(208, 19);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(208, 45);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(208, 72);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 128);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ekle-Düzenle Kısmı";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 99);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Şifre";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(59, 97);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(167, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Mail";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Adres";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(167, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Cep";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Telefon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Ad";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(382, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(382, 196);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 26);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Resim";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "HOCA EKRANI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ NetFitnessProject/Hoca Ekrani.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Security.Cryptography;
+
+namespace KullaniciGirisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        sporEntities1 spr = new sporEntities1();
+        int brnsID = -1;
+
+        ArrayList brid = new ArrayList();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IEnumerable<branslar> liste = spr.branslar;
+
+            foreach (var item in liste)
+            {
+                branslar br = new branslar();
+                br.bransAdi = item.bransAdi;
+                brnsID = item.bransID;
+                brid.Add(item.bransID);
+                comboBox1.Items.Add(br.bransAdi);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = spr.hocaGetirPro(comboBox1.SelectedItem.ToString());
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            pictureBox1.ImageLocation = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox1.Text.Trim()) && !String.IsNullOrEmpty(textBox2.Text.Trim()) && !String.IsNullOrEmpty(textBox3.Text.Trim()) && !String.IsNullOrEmpty(textBox4.Text.Trim()) && !String.IsNullOrEmpty(textBox5.Text.Trim()) && !String.IsNullOrEmpty(textBox6.Text.Trim()) && !String.IsNullOrEmpty(textBox7.Text.Trim())
+                )
+            {
+                try
+                {
+                    var veri1 = spr.hocalar.Where(f => f.mail == textBox6.Text.Trim()).FirstOrDefault();                  
+                    if (veri1!=null)
+                    {
+                        MessageBox.Show("Bu mail mevcut");
+                    }
+                    else
+                    {
+                        hocalar hc = new hocalar();
+                        hc.adminID = 1;
+                        hc.bransID = (int)brid[comboBox1.SelectedIndex];
+                        hc.adi = textBox1.Text;
+                        hc.soyadi = textBox2.Text;
+                        hc.telefon = textBox3.Text;
+                        hc.cep = textBox4.Text;
+                        hc.adres = textBox5.Text;
+                        hc.mail = textBox6.Text;
+                        hc.sifre = MD5Sifrele(textBox7.Text);
+                        hc.tarih = DateTime.Now;
+                        hc.resim = "Resimler\\" + resimYolu;
+                        spr.hocalar.Add(hc);
+                        spr.SaveChanges();
+                        comboBox1_SelectedIndexChanged(null, null);
+                        textBox1.Clear();
+                        textBox2.Clear();
+                        textBox3.Clear();
+                        textBox4.Clear();
+                        textBox5.Clear();
+                        textBox6.Clear();
+                        textBox7.Clear();
+                        pictureBox1.ImageLocation = "";
+                    }                    
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Branşı seçiniz.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tüm alanları Doldurunuz ve branşı seçiniz.");
+            }
+
+        }
+
+        int did = -1;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            did = Convert.ToInt32(id);
+            try
+            {
+                spr.hocalar.Remove(spr.hocalar.Find(did));
+                spr.SaveChanges();
+                comboBox1_SelectedIndexChanged(null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata : " + ex);
+            }
+        }
+
+        int did2 = -1;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox1.Text.Trim()) && !String.IsNullOrEmpty(textBox2.Text.Trim()) && !String.IsNullOrEmpty(textBox3.Text.Trim()) && !String.IsNullOrEmpty(textBox4.Text.Trim()) && !String.IsNullOrEmpty(textBox5.Text.Trim()) && !String.IsNullOrEmpty(textBox6.Text.Trim()))
+            {
+                try
+                {
+                    String id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                    did2 = Convert.ToInt32(id);
+
+                    hocalar hc = spr.hocalar.FirstOrDefault(sil => sil.hocaID == did2);
+                    if (hc != null)
+                    {
+                        hc.adminID = 1;
+                        hc.bransID = (int)brid[comboBox1.SelectedIndex];
+                        hc.adi = textBox1.Text;
+                        hc.soyadi = textBox2.Text;
+                        hc.telefon = textBox3.Text;
+                        hc.cep = textBox4.Text;
+                        hc.adres = textBox5.Text;
+                        hc.mail = textBox6.Text;
+                        hc.tarih = DateTime.Now;
+                        if (!String.IsNullOrEmpty(textBox7.Text.Trim()))
+                        {
+                            hc.sifre = MD5Sifrele(textBox7.Text);
+                        }                        
+                        hc.resim = "Resimler\\" + resimYolu;
+                        spr.SaveChanges();
+                        comboBox1_SelectedIndexChanged(null, null);
+                        textBox1.Clear();
+                        textBox2.Clear();
+                        textBox3.Clear();
+                        textBox4.Clear();
+                        textBox5.Clear();
+                        textBox6.Clear();
+                        textBox7.Clear();
+                        pictureBox1.ImageLocation = "";
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Branşı seçiniz.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Boş alan bırakmayınız.");
+            }
+        }
+        
+        string resimYolu;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "jpeg dosyası(*.jpg)|*.jpg|Bitmap(*.bmp)|*.bmp";//.bmp veya .jpg olarak kayıt imkanı sağlıyoruz.
+
+            openFileDialog1.Title = "Resim Ekranı";//diğaloğumuzun başlığını belirliyoruz.
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                resimYolu = openFileDialog1.SafeFileName;
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        public string MD5Sifrele(string metin)
+        {
+            // MD5CryptoServiceProvider nesnenin yeni bir instance'sını oluşturalım.
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+
+            //Girilen veriyi bir byte dizisine dönüştürelim ve hash hesaplamasını yapalım.
+            byte[] btr = Encoding.UTF8.GetBytes(metin);
+            btr = md5.ComputeHash(btr);
+
+            //byte'ları biriktirmek için yeni bir StringBuilder ve string oluşturalım.
+            StringBuilder sb = new StringBuilder();
+
+
+            //hash yapılmış her bir byte'ı dizi içinden alalım ve her birini hexadecimal string olarak formatlayalım.
+            foreach (byte ba in btr)
+            {
+                sb.Append(ba.ToString("x2").ToLower());
+            }
+
+            //hexadecimal(onaltılık) stringi geri döndürelim.
+            return sb.ToString();
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+                pictureBox1.Load("Resimler\\default.jpg");
+                textBox1.Text = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[1].Value.ToString();
+                textBox2.Text = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[2].Value.ToString();
+                textBox3.Text = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[3].Value.ToString();
+                textBox4.Text = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[4].Value.ToString();
+                textBox5.Text = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[5].Value.ToString();
+                textBox6.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+
+                String resim = spr.resimPro(id).First().ToString();
+                pictureBox1.Load(resim);
+
+                //string asd = Convert.ToString(spr.Database.ExecuteSqlCommand("select resim from hocalar where hocaID ='" + id + "'"));
+                //pictureBox1.Load(asd);
+
+                //pictureBox1.Load(spr.resimPro(id).ToString());
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
+
