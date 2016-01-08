751ef15f4e997f3fc8888460d07791b8dcd2fcaf
kubra15 <kubraakca1115@gmail.com>
2016-01-08 18:01:58 +0200

    hoca ayarları
    hocanın ayarlarını güncelldiği form

---
+++ NetFitnessProject/HocaAyarlari.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace NetFitnessProject
+{
+    public partial class HocaAyarlari : Screen
+    {
+        public HocaAyarlari()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                using (var context = new sporEntities())
+                {
+                    programlar pro = new programlar();
+
+                    if (!string.IsNullOrEmpty(txtMail.Text) && !string.IsNullOrEmpty(txtTel.Text) && !string.IsNullOrEmpty(txtCep.Text) && !string.IsNullOrEmpty(txtAdres.Text) && !string.IsNullOrEmpty(txtSifre.Text))
+                    {
+                        
+                      //  context.guncelleme(txtTel.Text,txtCep.Text,txtAdres.Text,txtMail.Text,txtSifre.Text);
+                        context.guncelleme(txtMail.Text, txtTel.Text, txtCep.Text, txtAdres.Text, txtSifre.Text);
+                    MessageBox.Show("Güncelleme Başarılı");
+                    }
+                    else
+                    {
+                      
+
+                        MessageBox.Show("Güncelleme İçin Alanları Doldurunuz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Güncelleme  Başarısız." + ex);
+            }
+
+           
+        }
+
+        private void HocaAyarlari_Load(object sender, EventArgs e)
+        {
+            sporEntities db = new sporEntities();
+            try
+            {
+                using (var context = new sporEntities())
+                {
+
+                    context.hocalars.SqlQuery("select *from hocalar where hocaID='" + hocaGiris.ID + "')");
+                    IEnumerable<hocalar> uyeListe = db.hocalars;
+                    ArrayList ls = new ArrayList();
+                    foreach (var item in uyeListe)
+                    {
+                        hocalar hc = new hocalar();
+                        hc.mail = item.mail;
+                        hc.telefon = item.telefon;
+                        hc.cep = item.cep;
+                        hc.adres = item.adres;
+                        hc.sifre = item.sifre;
+                        txtMail.Text = hc.mail;
+                        txtTel.Text = hc.telefon;
+                        txtCep.Text = hc.cep;
+                        txtAdres.Text = hc.adres;
+                        txtSifre.Text = hc.sifre;
+
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("HATA" + ex);
+            }
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            hocaGiris frm = new hocaGiris();
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ NetFitnessProject/HocaAyarlari.designer.cs
@@ -0,0 +1,189 @@
+﻿namespace NetFitnessProject
+{
+    partial class HocaAyarlari
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(306, 110);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(150, 20);
+            this.txtMail.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mail";
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(306, 162);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(150, 20);
+            this.txtTel.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefon";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(306, 209);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(150, 20);
+            this.txtCep.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Cep";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(306, 254);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(150, 20);
+            this.txtAdres.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Adres";
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+           
+            this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnGuncelle.Location = new System.Drawing.Point(306, 347);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(150, 67);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Location = new System.Drawing.Point(690, 15);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(75, 23);
+            this.btnCikis.TabIndex = 3;
+            this.btnCikis.Text = "ÇIKIŞ";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(306, 299);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(150, 20);
+            this.txtSifre.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(264, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Şifre";
+            // 
+            // HocaAyarlari
+            // 
+            this.AcceptButton = this.btnGuncelle;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtMail);
+            this.Name = "HocaAyarlari";
+            this.Text = "HocaAyarlari";
+            this.Load += new System.EventHandler(this.HocaAyarlari_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label5;
+    }
+}
