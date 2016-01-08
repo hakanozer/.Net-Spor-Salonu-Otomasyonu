bf65c0e7dee5e69dd12af3752359a7f0fe90c461
gzmmmgr <gzmgr.92@gmail.com>
2016-01-08 17:33:34 +0200

    Turnike Geçiş Formu
    Bir spor salonu otomasyonu masaüstü programının turnike geçiş formunun
    kodu yazılmıştır. Bu kısımda;veritabanına kayıtlı güncel üyenin turnike
    geçişinin sağlanması amaçlanmaktadır. SQL Server bağlantısı Entity
    Framework ile yapılmıştır. Aynı zamanda üyenin güncel olup olmamasına
    göre turnike geçişi sağlanması için “proUyeIDAra” adında bir prosedür
    yazılmıştır.

---
+++ NetFitnessProject/TurnikeGecis.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SporSalonuOtomasyonu
+{
+    partial class TurnikeGecis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Location = new System.Drawing.Point(652, 12);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(75, 37);
+            this.btnCikis.TabIndex = 2;
+            this.btnCikis.Text = "ÇIKIŞ";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 166);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 166);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(366, 157);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(166, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(363, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 207);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 98);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "turnike geçişi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblAdSoyad
+            // 
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Location = new System.Drawing.Point(396, 103);
+            this.lblAdSoyad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(0, 13);
+            this.lblAdSoyad.TabIndex = 11;
+            // 
+            // TurnikeGecis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.lblAdSoyad);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCikis);
+            this.Name = "TurnikeGecis";
+            this.Text = "TurnikeGecis";
+            this.Load += new System.EventHandler(this.TurnikeGecis_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblAdSoyad;
+    }
+}
---
+++ NetFitnessProject/TurnikeGecis.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace SporSalonuOtomasyonu
+{
+    public partial class TurnikeGecis : Form
+    {
+        DateTime uyeBitisTarihi;
+        bool deger = false;
+        public TurnikeGecis()
+        {
+            InitializeComponent();
+        }
+        sporEntities se = new sporEntities();
+        public int ID = 0;
+
+        private void btnTurnike_Click(object sender, EventArgs e)
+        {
+          
+       
+        }
+       
+
+        
+        
+
+        private void TurnikeGecis_Load(object sender, EventArgs e)
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            deger = false;
+            try
+            {
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    string path = "";
+                    lblAdSoyad.Text = "";
+                    using (var ct = new sporEntities())
+                    {
+                        int sayi = Convert.ToInt32(textBox1.Text);
+
+                        foreach (proUyeIDAra_Result item in ct.proUyeIDAra(sayi))
+                        {
+                            deger = true;
+                            uyeBitisTarihi = (DateTime)item.bitisTarihi;
+                            path = item.resim;
+                            lblAdSoyad.Text = item.adi.ToUpper().Trim() + " " + item.soyadi.ToUpper().Trim();
+
+                        }
+                    }
+                    try
+                    {
+                        pictureBox1.Load(path);
+                    }
+                    catch (Exception)
+                    {
+                        pictureBox1.Load("resimler\\photo.jpg");
+                    }
+
+                    int sonuc = DateTime.Compare(uyeBitisTarihi, DateTime.Now);
+                    if (sonuc == 1 && !string.IsNullOrEmpty(lblAdSoyad.Text.Trim()))
+                    {
+                        TimeSpan kalanGun = uyeBitisTarihi.Subtract(DateTime.Now);
+                        MessageBox.Show("Üye Giriş Başarılı.  " + lblAdSoyad.Text.ToUpper().Trim() + ": " + kalanGun.Days + " Gününüz Kaldı.");
+                    }
+                    if (deger==false)
+                    {
+                      if (!deger)
+                    {
+                        MessageBox.Show("Üye Bulunamadı");
+                        textBox1.Clear();
+                        textBox1.Focus();
+                    }
+                    }
+                    else
+                    {
+if (sonuc == -1)
+                    {
+                        MessageBox.Show("Üyelik Süreniz Dolmuştur. Lütfen Yenileyiniz! Son Giriş Tarihiniz: " + uyeBitisTarihi);
+                        textBox1.Clear();
+                        textBox1.Focus();
+                    }
+                    }
+                    
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex);
+            }
+        }
+    }
+}
