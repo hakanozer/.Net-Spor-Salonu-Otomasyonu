d35c189108fa0efc905bf2c8eebff52da95bb648
Receps <wolfmatevi@gmail.com>
2016-01-08 16:41:03 +0200

    SporSalonuOtomasyonu Grafikler
    sql de hazırladığım prosedure'lerle visual studio ortamında entity
    framework kullanarak bağlayarak chart grafkleriyle kullanıcının belirli
    tarihler arasında ki raporları grafiklerle görülmesini sağladım.

---
+++ NetFitnessProject/grafik.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GRAFİK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 59);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(12, 85);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(199, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 152);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(325, 398);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(338, 152);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(434, 398);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ NetFitnessProject/grafik.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace GRAFİK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.chart1.Series.Clear();
+            comboBox1.Items.Add("Satışlar");
+            comboBox1.Items.Add("Demirbaş");
+            comboBox1.Items.Add("cinsiyet");
+            comboBox1.Items.Add("En Cok Satılan Paketler");
+            comboBox1.Items.Add("En Cok Hitap Edilen Yaş ve Cinsiyetler");
+            comboBox1.Items.Add("Derslerin Doluluk Oranları");
+
+        }
+
+
+
+        public void doldur()
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            this.chart1.Titles.Clear();//Chart da varsayılan olarak gelen başlıkları temizliyoruz.
+            this.chart1.Series.Clear();//Chart da varsayılan olarak gelen series (Liste) temizliyoruz.
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+
+
+                using (var contex = new sporEntities())
+                {
+                    dataGridView1.DataSource = contex.satış(dateTimePicker1.Value, dateTimePicker2.Value);
+
+                    string[] x = new string[dataGridView1.Rows.Count];
+                    int[] y = new int[dataGridView1.Rows.Count];
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        x[i] = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                        y[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[6].Value);
+                    }
+                    this.chart1.Series.Add("satış");
+                    chart1.Series[0].Points.DataBindXY(x, y);
+                    chart1.Series[0].ChartType = SeriesChartType.Pie;
+                    chart1.Titles.Add("satışlar");
+                    chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+                    chart1.Legends[0].Enabled = true;
+                }
+
+
+
+
+            }
+            else
+
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    using (var contex = new sporEntities())
+                    {
+                        dataGridView1.DataSource = contex.Demirbas(dateTimePicker1.Value, dateTimePicker2.Value);
+
+                        string[] x = new string[dataGridView1.Rows.Count];
+                        int[] y = new int[dataGridView1.Rows.Count];
+                        for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                        {
+                            x[i] = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                            y[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value);
+                        }
+                        this.chart1.Series.Add("Demirbaş");
+                        chart1.Series[0].Points.DataBindXY(x, y);
+                        chart1.Series[0].ChartType = SeriesChartType.Pie;
+                        chart1.Titles.Add("Demirbaş");
+                        chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+                        chart1.Legends[0].Enabled = true;
+                    }
+
+                }
+                else
+                    if (comboBox1.SelectedIndex == 2)
+                    {
+                        using (var contex = new sporEntities())
+                        {
+                            dataGridView1.DataSource = contex.UyelerCinsiyett(dateTimePicker1.Value, dateTimePicker2.Value);
+
+                            string[] x = new string[dataGridView1.Rows.Count];
+                            int[] y = new int[dataGridView1.Rows.Count];
+                            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                            {
+                                x[i] = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                                y[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+                            }
+                            this.chart1.Series.Add("CİNSİYET");
+                            chart1.Series[0].Points.DataBindXY(x, y);
+                            chart1.Series[0].ChartType = SeriesChartType.Column;
+                            chart1.Titles.Add("En Çok Satılan Paketler");
+                            chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+                            chart1.Legends[0].Enabled = true;
+                        }
+
+                    }
+                    else
+                        if (comboBox1.SelectedIndex == 3)
+                        {
+                            using (var contex = new sporEntities())
+                            {
+                                dataGridView1.DataSource = contex.EnCokSatilanPaket(dateTimePicker1.Value, dateTimePicker2.Value);
+
+                                string[] x = new string[dataGridView1.Rows.Count];
+                                int[] y = new int[dataGridView1.Rows.Count];
+                                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                                {
+                                    x[i] = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                                    y[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value);
+                                }
+                                this.chart1.Series.Add("Paketler");
+                                chart1.Series[0].Points.DataBindXY(x, y);
+                                chart1.Series[0].ChartType = SeriesChartType.Pyramid;
+                                chart1.Titles.Add("En Çok Satılan Paketler");
+                                chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+                                chart1.Legends[0].Enabled = true;
+                            }
+                        }
+                        else
+                            if (comboBox1.SelectedIndex == 4)
+                            {
+                                using (var contex = new sporEntities())
+                                {
+                                    dataGridView1.DataSource = contex.UyeYasDagılımı(dateTimePicker1.Value, dateTimePicker2.Value);
+
+                                    string[] x = new string[dataGridView1.Rows.Count];
+                                    int[] y = new int[dataGridView1.Rows.Count];
+                                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                                    {
+                                        x[i] = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                                        y[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+                                    }
+                                    this.chart1.Series.Add("Yaş Dağılımı");
+                                    chart1.Series[0].Points.DataBindXY(x, y);
+                                    chart1.Series[0].ChartType = SeriesChartType.Line;
+                                    chart1.Titles.Add("En Çok Satılan Paketler");
+                                    chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = false;
+                                    chart1.Legends[0].Enabled = true;
+                                }
+
+                            }
+                            else
+                                if (comboBox1.SelectedIndex == 5)
+                                {
+                                    using (var contex = new sporEntities())
+                                    {
+                                        dataGridView1.DataSource = contex.doluluk(dateTimePicker1.Value, dateTimePicker2.Value);
+
+                                        string[] x = new string[dataGridView1.Rows.Count];
+                                        int[] y = new int[dataGridView1.Rows.Count];
+                                        for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                                        {
+                                            x[i] = dataGridView1.Rows[i].Cells[3].Value.ToString();
+                                            y[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[4].Value);
+                                        }
+                                        this.chart1.Series.Add("Doluluk Oranı");
+                                        chart1.Series[0].Points.DataBindXY(x, y);
+                                        chart1.Series[0].ChartType = SeriesChartType.Column;
+                                        chart1.Titles.Add("En Çok Satılan Paketler");
+                                        chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = false;
+                                        chart1.Legends[0].Enabled = true;
+                                    }
+
+                                }
+
+        }
+
+
+
+
+
+    }
+}
+
