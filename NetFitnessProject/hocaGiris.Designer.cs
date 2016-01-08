6bca5228605aa0d970c9af8a88fd6d9a72991cbe
EmineGun <emine1gu@gmail.com>
2016-01-08 17:26:48 +0200

    HocaGirişi
    hocanın giriş yaptığı ana form

---
+++ NetFitnessProject/hocaGiris.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace NetFitnessProject
+{
+    partial class hocaGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(hocaGiris));
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(280, 137);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(128, 22);
+            this.txtKullaniciAdi.TabIndex = 0;
+            this.txtKullaniciAdi.Text = "emine.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(194, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(280, 173);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(128, 22);
+            this.txtSifre.TabIndex = 0;
+            this.txtSifre.Text = "123";
+            this.txtSifre.TextChanged += new System.EventHandler(this.txtSifre_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(194, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre";
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGiris.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnGiris.Location = new System.Drawing.Point(246, 209);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(75, 30);
+            this.btnGiris.TabIndex = 2;
+            this.btnGiris.Text = "GİRİŞ";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            this.btnGiris.Enter += new System.EventHandler(this.btnGiris_Click);
+            this.btnGiris.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btnGiris_KeyPress);
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikis.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCikis.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnCikis.Location = new System.Drawing.Point(333, 209);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(75, 30);
+            this.btnCikis.TabIndex = 3;
+            this.btnCikis.Text = "ÇIKIŞ";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGiris;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "hoca girişi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.Button btnCikis;
+    }
+}
+
---
+++ NetFitnessProject/hocaGiris.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetFitnessProject;
+
+namespace NetFitnessProject
+{
+    public partial class hocaGiris : Form
+    {
+        public hocaGiris()
+        {
+            InitializeComponent();
+        StartPosition = FormStartPosition.CenterScreen;
+        }
+   public  static int ID = 0;
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+
+
+            if (!string.IsNullOrEmpty(txtKullaniciAdi.Text)&&!string.IsNullOrEmpty(txtSifre.Text))
+            {
+                  try
+            {
+                 string id =txtKullaniciAdi.Text;
+                 sporEntities db = new sporEntities();
+                 string query = (from c in db.hocalars
+                        where c.mail == id && c.sifre == txtSifre.Text
+                        select c.mail).FirstOrDefault();
+                 if (query != null)
+                {
+                    int query1 = (from c in db.hocalars
+                    where c.mail == id && c.sifre == txtSifre.Text
+                    select c.hocaID).FirstOrDefault();
+                ID = query1;  
+               MessageBox.Show("Giriş Başarılı");
+               hocasayfasi hc = new hocasayfasi();  //*****---- Hatalar Çıktııııı!!!!!!! YARDIM LÜTFEEENNN!!
+               hc.Show();
+               this.Hide();
+                }
+                 else
+                 {
+                  
+                     MessageBox.Show("Kullanıcı Adı veya Şifre Yanlış", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                 }
+         
+  
+ 
+            }
+            catch (Exception ex)
+            {
+                
+                 MessageBox.Show("Giriş Başarısız"+ex);
+            }
+            }
+            else
+            {
+                MessageBox.Show("Alanlar Boş Bırakılamaz","Hata",  MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+}
+        //md5 şifreleme not:hoca kaydını ben yapmadığım için şifrelemeyi yapmak zorunda değilim.
+
+        public static string MD5Sifrele(string metin)
+        {
+            // MD5CryptoServiceProvider nesnenin yeni bir instance'sını oluşturalım.
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+
+            //Girilen veriyi bir byte dizisine dönüştürelim ve hash hesaplamasını yapalım.
+            byte[] btr = Encoding.UTF8.GetBytes(metin);
+            btr = md5.ComputeHash(btr);
+
+            //byte'ları biriktirmek için yeni bir StringBuilder ve string oluşturalım.
+            StringBuilder sb = new StringBuilder();
+
+
+            //hash yapılmış her bir byte'ı dizi içinden alalım ve her birini hexadecimal string olarak formatlayalım.
+            foreach (byte ba in btr)
+            {
+                sb.Append(ba.ToString("x2").ToLower());
+            }
+
+            //hexadecimal(onaltılık) stringi geri döndürelim.
+            return sb.ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSifre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGiris_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        }
+        
+
+    }
