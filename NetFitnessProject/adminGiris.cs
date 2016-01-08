fbade30d6229b5b99cb1e8e049381c995e1c0212
Ceyda Yalçınkaya <yalcinkayar.91@gmail.com>
2016-01-08 17:50:34 +0200

    Admin Giriş Sayfası
    Admin girişinin denetlendiği sayfa yapıldı.

---
+++ NetFitnessProject/adminGiris.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Data.Common;
+using NetFitnessProject;
+
+namespace NetFitnessProject
+{
+    public partial class adminGiris : Form
+    {
+        public static int id = -1;
+        public adminGiris()
+        {
+            InitializeComponent();
+        }
+
+        sporEntities se = new sporEntities();
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            String adi = txtKullaniciAdi.Text.Trim();
+            String sifre = txtSifre.Text.Trim();
+
+            if (adi.Equals("") || sifre.Equals(""))
+            {
+                MessageBox.Show("Lütfen Tüm Alanları Doldurunuz");
+                txtKullaniciAdi.Focus();
+            }
+            else
+            {
+
+                if (kullaniciDogrula(adi, MD5Olustur(sifre)))
+                {
+                    index f2 = new index();
+                    f2.Show();
+                    this.Hide();
+                    //MessageBox.Show("Kullanıcı id = " + id);
+                }
+                else
+                {
+                    //MessageBox.Show("Kullanıcı Adı yada Şifre Hatalı");
+                    txtKullaniciAdi.Text = "";
+                    txtSifre.Text = "";
+                    txtKullaniciAdi.Focus();
+                }
+
+            }
+            if (rdbHocaGirisi.Checked)
+            {
+                hocaGiris frmForm1 = new hocaGiris();
+                frmForm1.Show();
+               
+            }
+        }
+        
+        private bool kullaniciDogrula(string kAdi, string kParola)
+        {
+
+            bool durum = false;
+            using (sporEntities db = new sporEntities())
+            {
+
+                List<adminGiri> data = db.Database.SqlQuery<adminGiri>("select *from adminGiris where kulAdi = '" + kAdi + "' and sifre = '" + kParola + "' ").ToList();
+
+                // admin girişi
+                Boolean durumi = false;
+                if (radioButton1.CanSelect) {
+                    foreach (adminGiri item in data)
+                    {
+                        durumi = true;
+                    }
+
+                    if (data != null && durumi)
+                    {
+                        adminGiris.id = data[0].adminID;
+                        return true;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Admin kullanıcı adı yada şifre hatalı");
+                        durum = false;
+                    }
+                    
+                }
+
+                /*
+                // hoca girişi
+                if (radioButton2.CanSelect)
+                {
+                    hocalar hc = (from c in db.hocalars where c.mail == kAdi && c.sifre == kParola select c).FirstOrDefault();
+                    if (hc != null)
+                    {
+                        adminGiris.id = hc.hocaID;
+                        durum = true;
+                    }
+                    else {
+                        MessageBox.Show("Hoca kullanıcı adı yada şifre hatalı");
+                        durum = false;
+                    }
+                }
+                 * 
+                 */
+
+
+            }
+            return durum;
+        }
+
+        public string MD5Olustur(string input)
+        {
+            MD5 md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        private void txtKullaniciAdi_Validated(object sender, EventArgs e)
+        {
+            if (txtKullaniciAdi.Text.Trim() == "")
+                errorProvider1.SetError(txtKullaniciAdi, "Kullanıcı Adı Girmelisiniz");
+
+            else
+                errorProvider1.SetError(txtKullaniciAdi, "");
+
+        }
+
+        private void txtSifre_Validated(object sender, EventArgs e)
+        {
+            if (txtSifre.Text.Trim() == "")
+                errorProvider1.SetError(txtSifre, "Şifreyi Girmelisiniz");
+
+            else
+                errorProvider1.SetError(txtSifre, "");
+        }
+
+        private void adminGiris_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void rdbHocaGirisi_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+}
---
+++ NetFitnessProject/adminGiris.designer.cs
@@ -0,0 +1,165 @@
+﻿namespace NetFitnessProject
+{
+    partial class adminGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(adminGiris));
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.rdbHocaGirisi = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(270, 137);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(152, 22);
+            this.txtKullaniciAdi.TabIndex = 5;
+            this.txtKullaniciAdi.Text = "ali";
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.Location = new System.Drawing.Point(270, 220);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(79, 30);
+            this.btnGiris.TabIndex = 9;
+            this.btnGiris.Text = "GİRİŞ";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(270, 166);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(152, 22);
+            this.txtSifre.TabIndex = 6;
+            this.txtSifre.Text = "12345";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(173, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Şifre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(173, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(270, 194);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 20);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Admin";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // rdbHocaGirisi
+            // 
+            this.rdbHocaGirisi.AutoSize = true;
+            this.rdbHocaGirisi.BackColor = System.Drawing.Color.Transparent;
+            this.rdbHocaGirisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbHocaGirisi.Location = new System.Drawing.Point(363, 193);
+            this.rdbHocaGirisi.Name = "rdbHocaGirisi";
+            this.rdbHocaGirisi.Size = new System.Drawing.Size(59, 20);
+            this.rdbHocaGirisi.TabIndex = 12;
+            this.rdbHocaGirisi.TabStop = true;
+            this.rdbHocaGirisi.Text = "Hoca";
+            this.rdbHocaGirisi.UseVisualStyleBackColor = false;
+            this.rdbHocaGirisi.CheckedChanged += new System.EventHandler(this.rdbHocaGirisi_CheckedChanged);
+            // 
+            // adminGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.rdbHocaGirisi);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "adminGiris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kullanıcı Girişi";
+            this.Load += new System.EventHandler(this.adminGiris_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rdbHocaGirisi;
+    }
+}
+
